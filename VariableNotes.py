f94879f0d81cf68b87e6565fabb1221d1f1dbba2
Xat Gomez <52943648+xatgomezstudio@users.noreply.github.com>
2022-01-21 17:25:57 -0400

    Add files via upload

---
+++ VariableNotes.py
@@ -0,0 +1,91 @@
+# Story variables and values
+
+user = "Bing Bong"
+age = "90"
+
+# indent
+print("\n")
+
+# integers
+print(2)
+
+# upper and lower case commands, and matching boolean command
+print(user.upper().isupper())
+
+# length of the variable string
+print(len(user))
+# returns the value of letter at that index
+print(user[0])
+# reports the index placement of the letter given
+print(user.index("ng"))
+# replace chosen string values
+print(user.replace("Bong", "Bob"))
+
+# indent
+print("\n")
+
+# story exercise with variables
+print("There once was a man named " + user + ",")
+print("he was " + age + " years old.")
+
+user = "Mike"
+
+print("He really liked the name " + user + ",")
+print("but he didn't like be ing " + age + " years old.")
+
+# story exercise with numbers
+# indent
+print("\n")
+
+age = 90
+
+# story exercise with variables
+print("There once was a man named " + user + ",")
+print("he was " + str(age) + " years old.")
+
+user = "Mike"
+
+print("He really liked the name " + user + ",")
+print("but he didn't like be ing " + str(age) + " years old.")
+
+# INTEGERS
+print("\n")
+
+num = 2
+# picks the maximum or minimum number
+print(min(3,9))
+print(max(2,5))
+
+print(round(4.59823710948710928347))
+
+# IMPORTING INFORMATION
+print("\n")
+from math import *
+
+# drops the deci
+print(floor(3.7))
+# rounds regardless
+print(ceil(3.8))
+
+# USER INPUT NOTES
+print("\n")
+# name = input("Name your dingle berry pet: ")
+# print("Meet " + name + "! :} ")
+
+# CALCULATOR exercise
+
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = float(num1) + float(num2)
+
+#print(result)
+
+# MADLIBS Game
+#color = input("Enter a color: ")
+#pluralNoun = input("Enter a plural noun: ")
+#celebrity = input("Enter a celebrity: ")
+
+#print("Roses are " + color)
+#print(pluralNoun + " are blue")
+#print("I love " + celebrity)
+
